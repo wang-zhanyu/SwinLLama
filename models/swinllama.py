e45a0bb1d65d5430831b34f043d66dfbe8ce7447
Zhanyu Wang <zwan0839@uni.sydney.edu.au>
2023-05-24 07:30:50 +0000

    first commit

---
+++ models/swinllama.py
@@ -0,0 +1,300 @@
+import os
+import json
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from transformers import LlamaForCausalLM, LlamaTokenizer
+from evalcap.bleu.bleu import Bleu
+from evalcap.rouge.rouge import Rouge
+from evalcap.cider.cider import Cider
+from evalcap.meteor.meteor import Meteor
+from transformers import SwinModel
+from peft import get_peft_model, LoraConfig, TaskType
+
+
+class SwinLLama(pl.LightningModule):
+    """
+    Swin-LLAMA model.
+    """
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.save_hyperparameters(args)
+
+        print('Loading SwinTransformer')
+        self.visual_encoder = SwinModel.from_pretrained("microsoft/swin-base-patch4-window7-224")
+        
+        if args.freeze_swin:
+            for name, param in self.visual_encoder.named_parameters():
+                param.requires_grad = False
+        print('Loading SwinTransformer Done')
+
+        print('Loading LLAMA')
+        self.llama_tokenizer = LlamaTokenizer.from_pretrained(args.llama_model, use_fast=False)
+        self.llama_tokenizer.pad_token = self.llama_tokenizer.eos_token
+
+        if args.low_resource:
+            self.llama_model = LlamaForCausalLM.from_pretrained(
+                args.llama_model,
+                torch_dtype=torch.float16,
+                load_in_8bit=True,
+                device_map="auto"
+            )
+        else:
+            self.llama_model = LlamaForCausalLM.from_pretrained(
+                args.llama_model,
+                torch_dtype=torch.float16,
+            )
+            
+        if args.use_lora:
+            self.embed_tokens = self.llama_model.model.embed_tokens
+            peft_config = LoraConfig(
+                task_type=TaskType.CAUSAL_LM, inference_mode=False, r=8, lora_alpha=16, lora_dropout=0.1
+            )
+            self.llama_model = get_peft_model(self.llama_model, peft_config)
+            self.llama_model.print_trainable_parameters()
+            print('Loading LLAMA LoRA Done')         
+        else:
+            self.embed_tokens = self.llama_model.model.embed_tokens
+            for name, param in self.llama_model.named_parameters():
+                param.requires_grad = False
+            print('Loading LLAMA Done')
+
+        self.llama_proj = nn.Linear(
+            self.visual_encoder.num_features, self.llama_model.config.hidden_size
+        )
+        self.layer_norm = nn.LayerNorm(self.llama_model.config.hidden_size)
+        self.max_txt_len = args.max_txt_len
+        self.end_sym = args.end_sym
+
+        if args.delta_file is not None:
+            state_dict = torch.load(args.delta_file, map_location=torch.device(f'cuda:{torch.cuda.current_device()}'))['model']
+            self.load_state_dict(state_dict=state_dict, strict=False)
+            print(f'Load checkpoint from {args.delta_file}')
+
+    def score(self, ref, hypo):
+        """
+        ref, dictionary of reference sentences (id, sentence)
+        hypo, dictionary of hypothesis sentences (id, sentence)
+        score, dictionary of scores
+        """
+        scorers = [
+            (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
+            (Rouge(), "ROUGE_L"),
+            (Meteor(), "METEOR"),
+            (Cider(), "CIDEr")
+        ]
+        final_scores = {}
+        for scorer, method in scorers:
+            score, scores = scorer.compute_score(ref, hypo)
+            if type(score) == list:
+                for m, s in zip(method, score):
+                    final_scores[m] = s
+            else:
+                final_scores[method] = score
+        return final_scores
+
+    def encode_img(self, image):
+        device = image.device
+        # image_embeds = self.visual_encoder(image)['last_hidden_state'].to(device)
+        image_embeds = self.visual_encoder(image)['pooler_output'].unsqueeze(1).to(device)
+        image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(device)
+
+        inputs_llama = self.llama_proj(image_embeds)
+        atts_llama = torch.ones(inputs_llama.size()[:-1], dtype=torch.long).to(image.device)
+        return inputs_llama, atts_llama
+
+    def prompt_wrap(self, img_embeds, atts_img, prompt='<Img><ImageHere></Img> Generate a diagnosis report for this chest xray image.'):
+        batch_size = img_embeds.shape[0]
+        p_before, p_after = prompt.split('<ImageHere>')
+        p_before_tokens = self.llama_tokenizer(
+            p_before, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
+        p_after_tokens = self.llama_tokenizer(
+            p_after, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
+        p_before_embeds = self.embed_tokens(p_before_tokens.input_ids).expand(batch_size, -1, -1)
+        p_after_embeds = self.embed_tokens(p_after_tokens.input_ids).expand(batch_size, -1, -1)
+        wrapped_img_embeds = torch.cat([p_before_embeds, img_embeds, p_after_embeds], dim=1)
+        wrapped_atts_img = atts_img[:, :1].expand(-1, wrapped_img_embeds.shape[1])
+        return wrapped_img_embeds, wrapped_atts_img
+
+    def forward(self, samples):
+        image = samples["image"]
+        img_embeds, atts_img = self.encode_img(image)
+        img_embeds = self.layer_norm(img_embeds)
+
+        img_embeds, atts_img = self.prompt_wrap(img_embeds, atts_img)
+
+        self.llama_tokenizer.padding_side = "right"
+        text = [t + self.end_sym for t in samples["input_text"]]
+
+        to_regress_tokens = self.llama_tokenizer(
+            text,
+            return_tensors="pt",
+            padding="longest",
+            truncation=True,
+            max_length=self.max_txt_len,
+            add_special_tokens=False
+        ).to(image.device)
+
+        targets = to_regress_tokens.input_ids.masked_fill(
+            to_regress_tokens.input_ids == self.llama_tokenizer.pad_token_id, -100
+        )
+
+        empty_targets = (
+            torch.ones([atts_img.shape[0], atts_img.shape[1]+1],
+                       dtype=torch.long).to(image.device).fill_(-100)  # plus one for bos
+        )
+        targets = torch.cat([empty_targets, targets], dim=1)
+
+        batch_size = img_embeds.shape[0]
+        bos = torch.ones([batch_size, 1],
+                         dtype=to_regress_tokens.input_ids.dtype,
+                         device=to_regress_tokens.input_ids.device) * self.llama_tokenizer.bos_token_id
+        bos_embeds = self.embed_tokens(bos)
+        atts_bos = atts_img[:, :1]
+
+        to_regress_embeds = self.embed_tokens(to_regress_tokens.input_ids)
+        inputs_embeds = torch.cat([bos_embeds, img_embeds, to_regress_embeds], dim=1)
+        attention_mask = torch.cat([atts_bos, atts_img, to_regress_tokens.attention_mask], dim=1)
+
+        outputs = self.llama_model(
+            inputs_embeds=inputs_embeds,
+            attention_mask=attention_mask,
+            return_dict=True,
+            labels=targets,
+        )
+        loss = outputs.loss
+        return {"loss": loss}
+
+    def training_step(self, batch, batch_idx):
+        result = self(batch)
+        # self.log_dict(to_log, prog_bar=True)
+        return result
+
+    def save_checkpoint(self, eval_res):
+        current_epoch, global_step = self.trainer.current_epoch, self.trainer.global_step
+        param_grad_dic = {
+            k: v.requires_grad for (k, v) in self.named_parameters() if v.requires_grad
+        }
+        state_dict = self.state_dict()
+        for k in list(state_dict.keys()):
+            if k not in param_grad_dic.keys():
+                del state_dict[k]
+        save_obj = {
+            "model": state_dict,
+            "config": self.hparams,
+            "epoch": current_epoch,
+            "step":global_step
+        }
+        os.makedirs(os.path.join(self.hparams.savedmodel_path, 'checkpoints'), exist_ok=True)
+        save_to = os.path.join(
+            self.hparams.savedmodel_path, 'checkpoints',
+            "checkpoint_epoch{}_step{}_bleu{:3f}_cider{:3f}.pth".format(current_epoch, global_step, eval_res['Bleu_4'], eval_res['CIDEr']),
+        )
+        self.print("Saving checkpoint at step {} to {}.".format(global_step, save_to))
+        torch.save(save_obj, save_to)
+    
+    def validation_step(self, samples, batch_idx):
+        ref = samples['input_text']
+        image = samples["image"]
+        img_embeds, atts_img = self.encode_img(image)
+        img_embeds = self.layer_norm(img_embeds)
+        img_embeds, atts_img = self.prompt_wrap(img_embeds, atts_img)
+
+        batch_size = img_embeds.shape[0]
+        bos = torch.ones([batch_size, 1],
+                         dtype=atts_img.dtype,
+                         device=atts_img.device) * self.llama_tokenizer.bos_token_id
+        bos_embeds = self.embed_tokens(bos)
+        atts_bos = atts_img[:, :1]
+
+        inputs_embeds = torch.cat([bos_embeds, img_embeds], dim=1)
+        attention_mask = torch.cat([atts_bos, atts_img], dim=1)
+
+        outputs = self.llama_model.generate(
+            inputs_embeds=inputs_embeds,
+            # stopping_criteria=self.stopping_criteria,
+            num_beams=self.hparams.beam_size,
+            do_sample=self.hparams.do_sample,
+            max_length=self.hparams.report_max_length,
+            repetition_penalty=self.hparams.repetition_penalty,
+            length_penalty=self.hparams.length_penalty,
+            temperature=self.hparams.temperature,
+        )
+        hypo = [self.decode(i) for i in outputs]
+        return hypo, ref
+    
+    def decode(self, output_token):
+        if output_token[0] == 0:  # the model might output a unknow token <unk> at the beginning. remove it
+            output_token = output_token[1:]
+        if output_token[0] == 1:  # some users find that there is a start token <s> at the beginning. remove it
+            output_token = output_token[1:]
+        output_text = self.llama_tokenizer.decode(output_token, add_special_tokens=False)
+        output_text = output_text.split('###')[0]  # remove the stop sign '###'
+        output_text = output_text.split('Radiologist:')[-1].strip()
+        return output_text
+    
+    def validation_epoch_end(self, outputs):
+        ref, hypo = [], []
+        if self.trainer.num_devices != 1:
+            allgather = self.all_gather(outputs)
+            for i in allgather:
+                hypo.extend(i[0])
+                ref.extend(i[1])
+        else:
+            for i in outputs:
+                hypo.extend(i[0])
+                ref.extend(i[1])
+        ref = {k:[v] for k, v in enumerate(ref)}
+        hypo = {k:[v] for k, v in enumerate(hypo)}
+        eval_res = self.score(ref=ref,hypo=hypo)
+        self.log_dict(eval_res, logger=True)
+
+        result_folder = os.path.join(self.hparams.savedmodel_path, 'result')
+        os.makedirs(result_folder, exist_ok=True)
+        current_epoch, global_step = self.trainer.current_epoch, self.trainer.global_step
+        json.dump(hypo, open(os.path.join(result_folder, f"result_{current_epoch}_{global_step}" + '.json'), 'w'))
+        json.dump(ref, open(os.path.join(result_folder, 'refs.json'), 'w'))
+        self.print(eval_res)
+        if self.trainer.local_rank == 0:
+            self.save_checkpoint(eval_res)
+        
+        # val_score = 0
+        # for score_type, weight in zip(self.hparams.scorer_types, self.hparams.weights):
+        #     val_score += eval_res[score_type] * weight
+
+        # if val_score > self.val_score:
+            # self.save_checkpoint(eval_res)
+            # self.val_score = val_score
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+        return optimizer
+
+    def get_progress_bar_dict(self):
+        # don't show the version number
+        items = super().get_progress_bar_dict()
+        items.pop("v_num", None)
+        return items
+
+    def optimizer_zero_grad(self, epoch, batch_idx, optimizer, optimizer_idx):
+        optimizer.zero_grad()
+
+
+if __name__ == "__main__":
+    import os
+    from PIL import Image
+    import numpy as np
+    from transformers import Blip2Processor
+    image_path = '/home/zhanyu_wang/code/visual-chatXrays/image/00c80722.png'
+    caption = 'impression: No acute cardiopulmonary process. Findings: Well-expanded lungs are clear.  There are no pleural effusions or pneumothorax.  The cardiomediastinal and hilar contours are normal.  Pulmonary vascularity is normal.  There is mild marginal spurring involving the visualized thoracolumbar spine.'
+    image = Image.open(image_path).convert("RGB")
+    processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+    inputs = processor(images=image, return_tensors="pt").to('cpu')
+    sample = {'image':inputs['pixel_values'], "text_input":caption}
+    
+    model = SwinLLama(llama_model="/home/zhanyu_wang/code/MiniGPT-4/vicuna_weights",
+                     prompt_path="prompts/alignment.txt",
+                     prompt_template='###Human: {} ###Assistant: ')
+    output = model(sample)
+    print(output)
